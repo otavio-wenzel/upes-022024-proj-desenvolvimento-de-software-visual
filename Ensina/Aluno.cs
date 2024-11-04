e94f7c599c17eaeb6853d5fa5953865c30bb0588
otavio-wenzel <otavio.wenzel@gmail.com>
2024-11-04 02:24:54 -0300

    Criação da classe Aluno

---
+++ Ensina/Aluno.cs
@@ -0,0 +1,28 @@
+
+using System.ComponentModel.DataAnnotations;
+
+public class Aluno
+{
+    public int Id { get; set; }
+
+    public string? Nome { get; set; }
+
+    public DateTime? DataDeNascimento { get; set; }
+
+    [Required(ErrorMessage = "RG é obrigatório")]
+    [RegularExpression(@"^\d+$", ErrorMessage = "O RG deve conter apenas dígitos numéricos")]
+    public string Rg { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Telefone é obrigatório")]
+    public string Telefone { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email é obrigatório")]
+    [EmailAddress(ErrorMessage = "Formato de email inválido")]
+    public string Email { get; set; } = string.Empty;
+
+    public string? NomeDoTutor { get; set; }
+
+    [Required(ErrorMessage = "RG do tutor é obrigatório")]
+    [RegularExpression(@"^\d+$", ErrorMessage = "O RG do tutor deve conter apenas dígitos numéricos")]
+    public string RgDoTutor { get; set; } = string.Empty;
+}
